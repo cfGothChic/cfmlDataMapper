4b61b0d002a267bd3b29387b9edfdaf1524adafc
Daria Norris <cfgothchic@gmail.com>
2017-12-10 14:15:43 -0500

    add sql for new user role tables

--- samples/sql/mssql.sql
+++ samples/sql/mssql.sql
@@ -22,6 +22,27 @@ GO
 ALTER TABLE [dbo].[departments] ADD  CONSTRAINT [DF_departments_createdate]  DEFAULT (getdate()) FOR [createdate]
 GO
 
+/****** Object:  Table [dbo].[roles]    Script Date: 8/24/2014 6:50:50 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[roles](
+	[roleId] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[createdate] [datetime] NOT NULL,
+	[updatedate] [datetime] NOT NULL,
+ CONSTRAINT [PK_roles] PRIMARY KEY CLUSTERED 
+(
+	[roleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[roles] ADD  CONSTRAINT [DF_roles_createdate]  DEFAULT (getdate()) FOR [createdate]
+GO
+
 /****** Object:  Table [dbo].[usertypes]    Script Date: 8/24/2014 6:50:50 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -80,21 +101,65 @@ GO
 ALTER TABLE [dbo].[users] CHECK CONSTRAINT [FK_users_usertypes]
 GO
 
+/****** Object:  Table [dbo].[users_roles]    Script Date: 12/10/2017 1:51:08 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[users_roles](
+	[userId] [int] NOT NULL,
+	[roleId] [int] NOT NULL,
+ CONSTRAINT [PK_users_roles] PRIMARY KEY CLUSTERED 
+(
+	[userId] ASC,
+	[roleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[users_roles]  WITH CHECK ADD  CONSTRAINT [FK_users_roles_roles] FOREIGN KEY([roleId])
+REFERENCES [dbo].[roles] ([roleId])
+GO
+ALTER TABLE [dbo].[users_roles] CHECK CONSTRAINT [FK_users_roles_roles]
+GO
+
+ALTER TABLE [dbo].[users_roles]  WITH CHECK ADD  CONSTRAINT [FK_users_roles_users] FOREIGN KEY([userId])
+REFERENCES [dbo].[users] ([userId])
+GO
+ALTER TABLE [dbo].[users_roles] CHECK CONSTRAINT [FK_users_roles_users]
+GO
+
 -- populate tables
-INSERT INTO departments (name, createdate, updatedate) VALUES ('Accounting', GETDATE(), GETDATE())
-INSERT INTO departments (name, createdate, updatedate) VALUES ('Development', GETDATE(), GETDATE())
-INSERT INTO departments (name, createdate, updatedate) VALUES ('Sales', GETDATE(), GETDATE())
-INSERT INTO departments (name, createdate, updatedate) VALUES ('Support', GETDATE(), GETDATE())
+INSERT INTO departments (name, createdate, updatedate) VALUES ('Accounting', GETDATE(), GETDATE());
+INSERT INTO departments (name, createdate, updatedate) VALUES ('Development', GETDATE(), GETDATE());
+INSERT INTO departments (name, createdate, updatedate) VALUES ('Sales', GETDATE(), GETDATE());
+INSERT INTO departments (name, createdate, updatedate) VALUES ('Support', GETDATE(), GETDATE());
 
-INSERT INTO usertypes (name, createdate, updatedate) VALUES ('Admin', GETDATE(), GETDATE())
-INSERT INTO usertypes (name, createdate, updatedate) VALUES ('Manager', GETDATE(), GETDATE())
-INSERT INTO usertypes (name, createdate, updatedate) VALUES ('User', GETDATE(), GETDATE())
+INSERT INTO roles (name, createdate, updatedate) VALUES ('Editor', GETDATE(), GETDATE());
+INSERT INTO roles (name, createdate, updatedate) VALUES ('Reviewer', GETDATE(), GETDATE());
+INSERT INTO roles (name, createdate, updatedate) VALUES ('Manager', GETDATE(), GETDATE());
 
-INSERT INTO users (firstName, lastName, email, departmentId, userTypeId, createdate, updatedate)
-VALUES ('Curly', 'Stooge', 'curly@stooges.com', 1, 1, GETDATE(), GETDATE())
-
-INSERT INTO users (firstName, lastName, email, departmentId, userTypeId, createdate, updatedate)
-VALUES ('Larry', 'Stooge', 'larry@stooges.com', 2, 2, GETDATE(), GETDATE())
+INSERT INTO usertypes (name, createdate, updatedate) VALUES ('Admin', GETDATE(), GETDATE());
+INSERT INTO usertypes (name, createdate, updatedate) VALUES ('Manager', GETDATE(), GETDATE());
+INSERT INTO usertypes (name, createdate, updatedate) VALUES ('User', GETDATE(), GETDATE());
 
 INSERT INTO users (firstName, lastName, email, departmentId, userTypeId, createdate, updatedate)
-VALUES ('Moe', 'Stooge', 'moe@stooges.com', 3, 3, GETDATE(), GETDATE())
+VALUES ('Curly', 'Stooge', 'curly@stooges.com', 1, 1, GETDATE(), GETDATE());
+
+INSERT INTO users (firstName, lastName, email, departmentId, userTypeId, createdate, updatedate)
+VALUES ('Larry', 'Stooge', 'larry@stooges.com', 2, 2, GETDATE(), GETDATE());
+
+INSERT INTO users (firstName, lastName, email, departmentId, userTypeId, createdate, updatedate)
+VALUES ('Moe', 'Stooge', 'moe@stooges.com', 3, 3, GETDATE(), GETDATE());
+
+INSERT INTO dbo.users_roles ( userId, roleId )
+VALUES
+	( 1, 1 )
+	,( 1, 2 )
+	,( 1, 3 )
+	,( 2, 1 )
+	,( 2, 2 )
+	,( 3, 1 )
+	,( 3, 3 );
